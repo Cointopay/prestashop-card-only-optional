445f73c52832bf29b11b8b60940da26fce838eb8
Taimoor Ali <taimoor.ali@therightsw.com>
2022-10-11 19:03:31 +0500

    updated to use common card statuses

--- cointopay_direct_cc_optional/controllers/front/validation.php
+++ cointopay_direct_cc_optional/controllers/front/validation.php
@@ -65,7 +65,7 @@ class Cointopay_Direct_Cc_OptionalValidationModuleFrontController extends Module
         $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
 
 
-        $this->module->validateOrder($cart->id, Configuration::get('COINTOPAY_DIRECT_CC_OPTIONAL_PENDING'), $total, $this->module->displayName, NULL, array(), (int)$currency->id, false, $customer->secure_key);
+        $this->module->validateOrder($cart->id, Configuration::get('COINTOPAY_DIRECT_CC_WAITING'), $total, $this->module->displayName, NULL, array(), (int)$currency->id, false, $customer->secure_key);
         $link = new Link();
         $success_url = '';
         $success_url = $link->getPageLink('order-confirmation', null, null, array(
